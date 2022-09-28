62753032e98678f0e5fc58804e4e2e0bd78c45c2
KAD105 <kazem_abdi@yahoo.com>
2022-09-28 16:39:11 +0200

    setting up the mongoDB

--- server/index.js
+++ server/index.js
@@ -2,3 +2,12 @@ import bodyParser from "body-parser";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://fullstack_flashcards_app:UOkgHMZt3SZNPKPT@cluster0.zo6mc77.mongodb.net/?retryWrites=true&w=majority";
+
+const port = process.env.PORT || 5000;
