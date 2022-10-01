cba4a6d49194e897f39c28ffc62f759dbc0261b0
KAD105 <kazem_abdi@yahoo.com>
2022-10-01 08:22:16 +0200

    refactor: post and form components gone under body component

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,14 @@ import React from "react";
 
 import Card from "./components/UI/Card";
 import Navbar from "./components/Navbar/Navbar";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
+import Body from "./components/Body/Body";
 import "./index.css";
 
 const App = () => {
   return (
     <Card>
       <Navbar />
-      <Posts />
-      <Form />
+      <Body />
     </Card>
   );
 };
